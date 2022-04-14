9255682b3333fb1cd0257310b073ef10a79578e2
Jivesh <ljivesh@gmail.com>
2022-04-14 10:20:34 +0530

    Array algorithms both search and sorting V2.0

--- ArraySearchAlgos.cpp
+++ ArraySearchAlgos.cpp
@@ -65,6 +65,65 @@ void bin_search()
     }
 }
 
+void select_sort()
+{
+    cout<<"\nPlease enter the size of the array: "; int n; cin>>n;
+    cout<<"\nPlease enter the array: "; int arr[n]; for(int i=0; i<n; i++) cin>>arr[i];
+
+    int temp=0;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(arr[i]<arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout<<"\nArray after selection sorting: "; for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+}
+
+void bubble_sort()
+{
+    cout<<"\nPlease enter the size of the array: "; int n; cin>>n;
+    cout<<"\nPlease enter the array: "; int arr[n]; for(int i=0; i<n; i++) cin>>arr[i];
+    
+    int temp = 0, count = 1;
+    for(int i=0; i<n; i++)
+    {
+
+        for(int j=0; j<n-count; j++)
+        if(arr[j]>arr[j+1])
+        {
+            temp = arr[j+1];
+            arr[j+1] = arr[j];
+            arr[j] = temp;
+        }
+        count++;
+    }
+    cout<<"Array after bubble sorting: "; for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+}
+
+void insert_sort()
+{
+    cout<<"\nPlease enter the size of the array: "; int n; cin>>n;
+    cout<<"\nPlease enter the array: "; int arr[n]; for(int i=0; i<n; i++) cin>>arr[i];
+    
+
+    for(int i=1; i<n; i++)
+    {
+        int current = arr[i];
+        int j=0;
+        for(j=i-1; arr[j]>current && j>=0; j--) arr[j+1]=arr[j];
+        arr[j+1] = current;
+    }
+    cout<<"\nArray after insertion sort is: "; for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+
+}
+
 int main()
 {
     cout<<"\n\tWelcome to the Program";
@@ -73,13 +132,18 @@ int main()
     cout<<"\nPlease select the algorithm:"
         <<"\n1.) Linear Search"
         <<"\n2.) Binary Search"
+        <<"\n3.) Selection sort"
+        <<"\n4.) Bubble sort"
+        <<"\n5.) Insertion sort"
         <<"\nKindly enter your input: "; int x; cin>>x;
 
     switch(x)
     {
         case 1: lin_search(); break;
         case 2: bin_search(); break;
-            
+        case 3: select_sort(); break;
+        case 4: bubble_sort(); break;
+        case 5: insert_sort(); break;
     }
 
     cout<<endl;
